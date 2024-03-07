3cb0730b254a44a862ad4d9c64bc962a192c999d
gabes12 <gabriel.costa@gec.inatel.br>
2024-03-07 17:27:58 -0300

    [Entrega] pratica-0

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "[Gabriel Costa] - [1586]";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+    mensagem: 'Testando modulo: [Gabriel Costa] - [1586] !',
+    saudacao: function() {
+      console.log(this.mensagem);
+    }
+  };
